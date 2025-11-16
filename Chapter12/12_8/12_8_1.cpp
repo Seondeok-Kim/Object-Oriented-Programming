1f0110d9bd2c123061b5ab52321c936a82658929
seon deok Kim <76850241+Seondeok-Kim@users.noreply.github.com>
2025-11-16 23:38:02 +0900

    Add files via upload

---
+++ Chapter12/12_8/12_8_1.cpp
@@ -0,0 +1,62 @@
+// Virtual base classes (가상 기본 클래스)와 다이아몬드 문제
+// 부모 클래스 A, 자식 클래스 B1, B2가 있을 때
+// 부모 클래스 앞에 virtual을 지정해주지 않으면
+// 클래스 A가 2개가 생성되는 오류가 발생함
+
+#include <iostream>
+using namespace std;
+
+class PoweredDevice
+{
+public:
+	int m_i;
+
+	PoweredDevice(int power)
+	{
+		cout << "PowerDevice: " << power << '\n';
+	}
+};
+
+class Scanner : public PoweredDevice
+{
+public:
+	Scanner(int scanner, int power)
+		: PoweredDevice(power)
+	{
+		cout << "Scanner: " << scanner << endl;
+	}
+};
+
+class Printer : public PoweredDevice
+{
+public:
+	Printer(int printer, int power)
+		: PoweredDevice(power)
+	{
+		cout << "Printer: " << printer << endl;
+	}
+};
+
+
+class Copier : public Scanner, public Printer
+{
+public:
+	// Constructor Initializer Liset
+	Copier(int scanner, int printer, int power)
+		: Scanner(scanner, power), Printer(printer, power)
+	{}
+
+};
+
+int main()
+{
+	// 생성자 호출
+	Copier cop(1, 2, 3);
+
+	// 주소 출력 -> 두 PoweredDevice의 m_i의 주소가 다르다
+	// 부모 클래스 상속 시 virtual이 없으므로 PoweredDevice 2번 호출됨
+	// cop에서 Scanner로 PoweredDevice를 타고 m_i의 주소 출력
+	cout << &cop.Scanner::PoweredDevice::m_i << endl; // 부모 클래스 1번 출력
+	// cop에서 Printer로 PoweredDevice를 타고 m_i의 주소 출력
+	cout << &cop.Printer::PoweredDevice::m_i << endl; // 부모 클래스 1번 출력
+}
