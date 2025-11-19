a5450f8b51c3aeb96613bcc00f9fddc0754102b1
seon deok Kim <76850241+Seondeok-Kim@users.noreply.github.com>
2025-11-19 19:55:41 +0900

    Add files via upload

---
+++ Chapter14/14_02/14_2_1.cpp
@@ -0,0 +1,80 @@
+// 예외 처리와 스택 되감기(Stack Unwinding)
+#include <iostream>
+using namespace std;
+
+
+
+void last()
+{
+	cout << "last " << endl;
+	cout << "Throws exception" << endl;
+
+	throw - 1;
+
+	cout << "End last" << endl;
+}
+
+
+void third()
+{
+	cout << "Third" << endl;
+	last();
+	cout << "End third" << endl;
+}
+
+
+void second()
+{
+	cout << "Second" << endl;
+	try
+	{
+		third();
+	}
+	catch (double)
+	{
+		cerr << "Second caught double exception" << endl;
+	}
+	cout << "End second" << endl;
+}
+
+
+void first()
+{
+
+	cout << "First" << endl;
+	try
+	{
+		second();
+	}
+	catch (int)
+	{
+		cerr << "first caught int exception" << endl;
+	}
+
+	cout << "End first " << endl;
+}
+
+
+int main()
+{
+	cout << "Start" << endl;
+
+	try
+	{
+		first();
+	}
+	catch (int)
+	{
+		cerr << "main caught int exception" << endl;
+	}
+	// uncaught exceptions
+
+	// catch (....) // catch-all handlers
+	//{
+	//    cerr << "main caught ellipses exception" << endl;
+	//}
+
+	cout << "End main" << endl;
+
+	return 0;
+}
